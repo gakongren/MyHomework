2cd43c71f91add67b55378e7929f8aab0f7983d2
gakongren <gakongren@gmail.com>
2017-01-08 19:09:27 +0800

    Last update for exp7 On branch master

---
+++ src/cn/sorato/exp7/ManagementCore.java
@@ -0,0 +1,7 @@
+package cn.sorato.exp7;
+
+/**
+ * Created by kongren on 2017/1/5.
+ */
+public class ManagementCore{
+}
--- src/cn/sorato/exp7/info/ScoreInfo.java
+++
@@ -1,7 +0,0 @@
-package cn.sorato.exp7.info;
-
-/**
- * Created by kongren on 2016/11/15.
- */
-public class ScoreInfo{
-}
